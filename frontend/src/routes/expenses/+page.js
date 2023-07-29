41ed955e0f3eb8b20b628261a4ddc0ed282b2762
Maurice <info@heymann.dev>
2023-07-29 18:18:21 +0200

    added skeleton

--- frontend/src/routes/expenses/+page.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-import {API_URL} from '$env/static/private';
-
-export async function load({}) {
-    const res = await fetch(`${API_URL}/expenses`);
-    const item = await res.json();
-
-    return { item };
-}
